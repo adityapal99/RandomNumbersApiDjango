96990c7db3d983a67b79ea251c28f974b7bc38a4
adityapal99 <adityapal2000.2013@gmail.com>
2021-07-08 18:01:12 +0530

    heroku deploy

--- server/settings.py
+++ server/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ddhbhmqdip8emj',
+        'USER': 'ymystjodcfcrmq',
+        'PASSWORD': '7dc746a5f1bcd8a95779cfc518bf9f8d711d6cf709241ac06e002b2de8d09811',
+        'HOST': 'ec2-3-89-0-52.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
